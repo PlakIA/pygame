85688d6eb67458452bad87721675899958aebc06
Iaroslav Plaksin <Plak.I.A@yandex.ru>
2023-12-09 22:54:52 +0300

    Добавлены начальные файлы

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+
+import pygame
+
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill((255, 255, 255))
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
